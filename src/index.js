568d98db4c5aef8ef8db40783cfee673f4ab2937
Vicker <vcalero2001@gmail.com>
2025-07-03 03:25:53 +0200

    Impoted form-control file into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import "./styles/styles.css";
 import {ProjectsListManagement} from "./js-files/manage-projects";
-
-const projectAdd = document.querySelector(".add-project")
-projectAdd.addEventListener("click", () =>{
-    
-});
+import "./js-files/form-control.js";
 
 ProjectsListManagement.addProject("Gym");
 ProjectsListManagement.addProject("Games");
