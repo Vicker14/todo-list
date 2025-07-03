aaab3dd56acbfabca0a6737aeba21fff69ec1738
Vicker <vcalero2001@gmail.com>
2025-07-03 04:06:26 +0200

    Added a button to close project add form

--- src/js-files/form-control.js
+++ src/js-files/form-control.js
@@ -5,9 +5,16 @@ const form = document.querySelector(".newProjectForm form");
 
 const addProjectButton = document.querySelector(".add-project");
 
+const closeForm = form.querySelector(".close-form");
+
 addProjectButton.addEventListener("click", () => {
     divForm.classList.toggle("hidden");
 })
+
+closeForm.addEventListener("click", () => {
+    divForm.classList.add("hidden");
+    form.reset();
+});
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
