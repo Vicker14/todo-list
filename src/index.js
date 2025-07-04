40f76c159e63a884c99919c77c605a9db84b4e26
Vicker <vcalero2001@gmail.com>
2025-07-04 03:12:20 +0200

    Linked the todos form into index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./styles/styles.css";
 import {ProjectsListManagement} from "./js-files/manage-projects";
 import "./js-files/form-control.js";
+import {TodoTask} from "./js-files/task-creation.js";
+import "./js-files/form-add-todo.js";
+
+const hola = new TodoTask("Hola", "hello", "Uni", "12-3", "high");
 
 ProjectsListManagement.addProject("Gym");
 ProjectsListManagement.addProject("Games");
